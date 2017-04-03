f957b08090f6bd610db9271d6a491194cad28660
Jose Dolz <jose.dolz.upv@gmail.com>
2017-04-03 13:43:21 -0400

    Update networkSegmentation.py

--- src/networkSegmentation.py
+++ src/networkSegmentation.py
@@ -1,6 +1,33 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
 import sys
 
-from LiviaNet.startTesting import segmentVolume
+
+from LiviaNet.startTesting import startTesting
 
 def printUsage(error_type):
     if error_type == 1:
@@ -26,8 +53,8 @@ def networkSegmentation(argv):
 
     configIniName = argv[0]
     networkModelName = argv[1]
-   
-    segmentVolume(networkModelName,configIniName)
+    
+    startTesting(networkModelName,configIniName)
     print(" ***************** SEGMENTATION DONE!!! ***************** ")
   
    
