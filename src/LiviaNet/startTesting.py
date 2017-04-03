4ea31957e849d3ab2fe19a7172dbc99930a29ebb
Jose Dolz <jose.dolz.upv@gmail.com>
2017-04-03 11:47:35 -0400

    Add files via upload

---
+++ src/LiviaNet/startTesting.py
@@ -0,0 +1,287 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
+import numpy as np
+import time
+import os
+import pdb
+
+from Modules.General.Evaluation import computeDice
+from Modules.General.Utils import getImagesSet
+from Modules.General.Utils import load_model_from_gzip_file
+from Modules.IO.ImgOperations.imgOp import applyUnpadding
+from Modules.IO.loadData import load_imagesSinglePatient
+from Modules.IO.saveData import saveImageAsNifti
+from Modules.IO.saveData import saveImageAsMatlab
+from Modules.IO.sampling import *
+from Modules.Parsers.parsersUtils import parserConfigIni
+
+
+def segmentVolume(myNetworkModel,
+                  i_d,
+                  imageNames_Test,
+                  names_Test,
+                  groundTruthNames_Test,
+                  roiNames_Test,
+                  imageType,
+                  padInputImagesBool,
+                  receptiveField, 
+                  sampleSize_Test,
+                  strideVal,
+                  batch_Size,
+                  task # Validation (0) or testing (1)
+                  ):
+        # Get info from the network model        
+        networkName        = myNetworkModel.networkName
+        folderName         = myNetworkModel.folderName
+        n_classes          = myNetworkModel.n_classes
+        sampleSize_Test    = myNetworkModel.sampleSize_Test
+        receptiveField     = myNetworkModel.receptiveField  
+        outputShape        = myNetworkModel.lastLayer.outputShapeTest[2:] 
+        batch_Size         = myNetworkModel.batch_Size
+        padInputImagesBool = True
+    
+        # Get half sample size
+        sampleHalf = []
+        for h_i in range(3):
+            sampleHalf.append((receptiveField[h_i]-1)/2)
+        
+        # Load the images to segment
+        [imgSubject,  
+        gtLabelsImage, 
+        roi, 
+        paddingValues] = load_imagesSinglePatient(i_d,
+                                                  imageNames_Test,
+                                                  groundTruthNames_Test,
+                                                  roiNames_Test,
+                                                  padInputImagesBool,
+                                                  receptiveField, 
+                                                  sampleSize_Test,
+                                                  imageType, 
+                                                  )
+                                                  
+                                  
+        # Get image dimensions                                                    
+        imgDims = list(imgSubject.shape)
+    
+        [ sampleCoords ] = sampleWholeImage(imgSubject,
+                                            roi,
+                                            sampleSize_Test,
+                                            strideVal,
+                                            batch_Size
+                                            )
+        
+        numberOfSamples = len(sampleCoords)
+        sampleID = 0
+        numberOfBatches = numberOfSamples/batch_Size
+
+        #The probability-map that will be constructed by the predictions.
+        probMaps = np.zeros([n_classes]+imgDims, dtype = "float32")
+        
+        # Run over all the batches 
+        for b_i in xrange(numberOfBatches) :
+                 
+            # Get samples for batch b_i
+            
+            sampleCoords_b = sampleCoords[ b_i*batch_Size : (b_i+1)*batch_Size ]
+            
+            [imgSamples] = extractSamples(imgSubject,
+                                          sampleCoords_b,
+                                          sampleSize_Test,
+                                          receptiveField)
+
+            # Load the data of the batch on the GPU
+            myNetworkModel.testingData_x.set_value(imgSamples, borrow=True)
+           
+            # Call the testing Theano function            
+            predictions = myNetworkModel.networkModel_Test(0)
+            
+            predOutput = predictions[-1]
+            
+            # --- Now we can generate the probability maps from the predictions ----
+            # Run over all the regions
+            for r_i in xrange(batch_Size) :
+ 
+                sampleCoords_i = sampleCoords[sampleID]
+                coords = [ sampleCoords_i[0][0], sampleCoords_i[1][0], sampleCoords_i[2][0] ]
+
+                # Get the min and max coords
+                xMin = coords[0] + sampleHalf[0]
+                xMax = coords[0] + sampleHalf[0] + strideVal[0]
+
+                yMin = coords[1] + sampleHalf[1]
+                yMax = coords[1] + sampleHalf[1] + strideVal[1]
+
+                zMin = coords[2] + sampleHalf[2]
+                zMax = coords[2] + sampleHalf[2] + strideVal[2]
+                
+                probMaps[:,xMin:xMax, yMin:yMax, zMin:zMax] = predOutput[r_i]
+
+                sampleID += 1
+            
+        # Release data
+        myNetworkModel.testingData_x.set_value(np.zeros([1,1,1,1,1], dtype="float32"))
+
+        # Segmentation has been done in this point.
+        
+        # Now: Save the data
+        # Get the segmentation from the probability maps ---
+        segmentationImage = np.argmax(probMaps, axis=0) 
+        
+        #Save Result:
+        npDtypeForPredictedImage = np.dtype(np.int16)
+        suffixToAdd = "_Segm"
+ 
+        # Apply unpadding if specified
+        if padInputImagesBool == True:
+            segmentationRes = applyUnpadding(segmentationImage, paddingValues)
+        else:
+            segmentationRes = segmentationImage
+
+        # Generate folders to store the model
+        BASE_DIR = os.getcwd()
+        path_Temp = os.path.join(BASE_DIR,'outputFiles')
+
+        # For the predictions
+        predlFolderName = os.path.join(path_Temp,myNetworkModel.folderName)
+        predlFolderName = os.path.join(predlFolderName,'Pred')
+        if task == 0:
+            predTestFolderName = os.path.join(predlFolderName,'Validation')
+        else:
+            predTestFolderName = os.path.join(predlFolderName,'Testing')
+        
+        nameToSave = predTestFolderName + '/Segmentation_'+ names_Test[i_d]
+        
+        # Save Segmentation image
+        
+        print(" ... Saving segmentation result..."),
+        if imageType == 0: # nifti
+            imageTypeToSave = np.dtype(np.int16)
+            saveImageAsNifti(segmentationRes,
+                             nameToSave,
+                             imageNames_Test[i_d],
+                             imageTypeToSave)
+        else: # Matlab
+            # Cast to int8 for saving purposes
+            saveImageAsMatlab(segmentationRes.astype('int8'),
+                              nameToSave)
+
+
+        # Save the prob maps for each class (except background)
+        for c_i in xrange(1, n_classes) :
+            
+            
+            nameToSave = predTestFolderName + '/ProbMap_class_'+ str(c_i) + '_' + names_Test[i_d] 
+
+            probMapClass = probMaps[c_i,:,:,:]
+
+            # Apply unpadding if specified
+            if padInputImagesBool == True:
+                probMapClassRes = applyUnpadding(probMapClass, paddingValues)
+            else:
+                probMapClassRes = probMapClass
+
+            print(" ... Saving prob map for class {}...".format(str(c_i))),
+            if imageType == 0: # nifti
+                imageTypeToSave = np.dtype(np.float32)
+                saveImageAsNifti(probMapClassRes,
+                                 nameToSave,
+                                 imageNames_Test[i_d],
+                                 imageTypeToSave)
+            else:
+                # Cast to float32 for saving purposes
+                saveImageAsMatlab(segmentationRes.astype('float32'),
+                                  nameToSave)
+
+        # If segmentation done during evaluation, get dice
+        if task == 0:
+            print(" ... Computing Dice scores: ")
+            DiceArray = computeDice(segmentationImage,gtLabelsImage)
+            for d_i in xrange(len(DiceArray)):
+                print(" -------------- DSC (Class {}) : {}".format(str(d_i+1),DiceArray[d_i]))
+
+""" Main segmentation function """
+def startTesting(networkModelName,
+                 configIniName
+                 ) :
+
+    padInputImagesBool = True # from config ini
+    print " ******************************************  STARTING SEGMENTATION ******************************************"
+
+    print " **********************  Starting segmentation **********************"
+    myParserConfigIni = parserConfigIni()
+    myParserConfigIni.readConfigIniFile(configIniName,2)
+    
+
+    print " -------- Images to segment -------------"
+
+    print " -------- Reading Images names for segmentation -------------"
+    
+    # -- Get list of images used for testing -- #
+    (imageNames_Test, names_Test) = getImagesSet(myParserConfigIni.imagesFolder,myParserConfigIni.indexesToSegment)  # Images
+    (groundTruthNames_Test, gt_names_Test) = getImagesSet(myParserConfigIni.GroundTruthFolder,myParserConfigIni.indexesToSegment) # Ground truth
+    (roiNames_Test, roi_names_Test) = getImagesSet(myParserConfigIni.ROIFolder,myParserConfigIni.indexesToSegment) # ROI
+
+    # --------------- Load my LiviaNet3D object  --------------- 
+    print (" ... Loading model from {}".format(networkModelName))
+    myLiviaNet3D = load_model_from_gzip_file(networkModelName)
+    print " ... Network architecture successfully loaded...."
+
+    # Get info from the network model        
+    networkName        = myLiviaNet3D.networkName
+    folderName         = myLiviaNet3D.folderName
+    n_classes          = myLiviaNet3D.n_classes
+    sampleSize_Test    = myLiviaNet3D.sampleSize_Test
+    receptiveField     = myLiviaNet3D.receptiveField  
+    outputShape        = myLiviaNet3D.lastLayer.outputShapeTest[2:] 
+    batch_Size         = myLiviaNet3D.batch_Size
+    padInputImagesBool = myParserConfigIni.applyPadding
+    imageType          = myParserConfigIni.imageTypes
+    numberImagesToSegment = len(imageNames_Test)
+    
+    strideValues = myLiviaNet3D.lastLayer.outputShapeTest[2:]
+
+    # Run over the images to segment   
+    for i_d in xrange(numberImagesToSegment) :
+        print("**********************  Segmenting subject: {} ....total: {}/{}...**********************".format(names_Test[i_d],str(i_d+1),str(numberImagesToSegment)))
+        
+        segmentVolume(myLiviaNet3D,
+                  i_d,
+                  imageNames_Test,  # Full path
+                  names_Test,       # Only image name
+                  groundTruthNames_Test,
+                  roiNames_Test,
+                  imageType,
+                  padInputImagesBool,
+                  receptiveField, 
+                  sampleSize_Test,
+                  strideValues,
+                  batch_Size,
+                  1 # Validation (0) or testing (1)
+                  )
+                         
+       
+    print(" **************************************************************************************************** ")
---
+++ src/LiviaNet/startTraining.py
@@ -0,0 +1,218 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
+import sys
+import time
+import numpy as np
+import random
+import math
+import os
+                  
+from Modules.General.Utils import getImagesSet
+from Modules.General.Utils import dump_model_to_gzip_file
+from Modules.General.Utils import load_model_from_gzip_file
+from Modules.General.Evaluation import computeDice
+from Modules.IO.sampling import getSamplesSubepoch
+from Modules.Parsers.parsersUtils import parserConfigIni
+from startTesting import segmentVolume
+import pdb
+
+
+def startTraining(networkModelName,configIniName):
+    print " ************************************************  STARTING TRAINING **************************************************"
+    print " **********************  Starting training model (Reading parameters) **********************"
+
+    myParserConfigIni = parserConfigIni()
+   
+    myParserConfigIni.readConfigIniFile(configIniName,1)
+    
+    # Image type (0: Nifti, 1: Matlab)
+    imageType = myParserConfigIni.imageTypesTrain
+
+    print (" --- Do training in {} epochs with {} subEpochs each...".format(myParserConfigIni.numberOfEpochs, myParserConfigIni.numberOfSubEpochs))
+    print "-------- Reading Images names used in training/validation -------------"
+
+    # -- Get list of images used for training -- #
+    (imageNames_Train, names_Train)          = getImagesSet(myParserConfigIni.imagesFolder,myParserConfigIni.indexesForTraining)  # Images
+    (groundTruthNames_Train, gt_names_Train) = getImagesSet(myParserConfigIni.GroundTruthFolder,myParserConfigIni.indexesForTraining) # Ground truth
+    (roiNames_Train, roi_names_Train)        = getImagesSet(myParserConfigIni.ROIFolder,myParserConfigIni.indexesForTraining) # ROI
+    
+    # -- Get list of images used for validation -- #
+    (imageNames_Val, names_Val)          = getImagesSet(myParserConfigIni.imagesFolder,myParserConfigIni.indexesForValidation)  # Images
+    (groundTruthNames_Val, gt_names_Val) = getImagesSet(myParserConfigIni.GroundTruthFolder,myParserConfigIni.indexesForValidation) # Ground truth
+    (roiNames_Val, roi_names_Val)        = getImagesSet(myParserConfigIni.ROIFolder,myParserConfigIni.indexesForValidation) # ROI
+
+    # Print names
+    print " ================== Images for training ================"
+    for i in range(0,len(names_Train)):
+       print(" Image({}): {}  |  GT: {}  |  ROI {} ".format(i,names_Train[i], gt_names_Train[i], roi_names_Train[i] ))
+
+    print " ================== Images for validation ================"
+    for i in range(0,len(names_Val)):
+       print(" Image({}): {}  |  GT: {}  |  ROI {} ".format(i,names_Val[i], gt_names_Val[i], roi_names_Val[i] ))
+
+    print " ==============================================================="
+   
+    # --------------- Load my LiviaNet3D object  --------------- 
+    print (" ... Loading model from {}".format(networkModelName))
+    myLiviaNet3D = load_model_from_gzip_file(networkModelName)
+    print " ... Network architecture successfully loaded...."
+
+    # Asign parameters to loaded Net
+    myLiviaNet3D.numberOfEpochs = myParserConfigIni.numberOfEpochs
+    myLiviaNet3D.numberOfSubEpochs = myParserConfigIni.numberOfSubEpochs
+    myLiviaNet3D.numberOfSamplesSupEpoch  = myParserConfigIni.numberOfSamplesSupEpoch
+    
+    numberOfEpochs = myLiviaNet3D.numberOfEpochs
+    numberOfSubEpochs = myLiviaNet3D.numberOfSubEpochs
+    numberOfSamplesSupEpoch = myLiviaNet3D.numberOfSamplesSupEpoch
+    
+    # --------------- --------------  --------------- 
+    # --------------- Start TRAINING  --------------- 
+    # --------------- --------------  --------------- 
+    # Get sample dimension values
+    receptiveField = myLiviaNet3D.receptiveField
+    sampleSize_Train = myLiviaNet3D.sampleSize_Train
+
+    trainingCost = []
+
+    if myParserConfigIni.applyPadding == 1:
+        applyPadding = True
+    else:
+        applyPadding = False
+        
+    # Run over all the (remaining) epochs and subepochs
+    for e_i in xrange(numberOfEpochs):
+        # Recover last trained epoch
+        numberOfEpochsTrained = myLiviaNet3D.numberOfEpochsTrained
+                                        
+        print(" ============== EPOCH: {}/{} =================".format(numberOfEpochsTrained+1,numberOfEpochs))
+
+        costsOfEpoch = []
+        
+        for subE_i in xrange(numberOfSubEpochs): 
+            epoch_nr = subE_i+1
+            print (" --- SubEPOCH: {}/{}".format(epoch_nr,myLiviaNet3D.numberOfSubEpochs))
+
+            # Get all the samples that will be used in this sub-epoch
+            [imagesSamplesAll,
+            gt_samplesAll] = getSamplesSubepoch(numberOfSamplesSupEpoch,
+                                                imageNames_Train,
+                                                groundTruthNames_Train,
+                                                roiNames_Train,
+                                                imageType,
+                                                sampleSize_Train,
+                                                receptiveField,
+                                                applyPadding
+                                                )
+
+            # Variable that will contain weights for the cost function
+            # --- In its current implementation, all the classes have the same weight
+            weightsCostFunction = np.ones(myLiviaNet3D.n_classes, dtype='float32')
+               
+            numberBatches = len(imagesSamplesAll) / myLiviaNet3D.batch_Size 
+            
+            myLiviaNet3D.trainingData_x.set_value(imagesSamplesAll, borrow=True)
+            myLiviaNet3D.trainingData_y.set_value(gt_samplesAll, borrow=True)
+                 
+            costsOfBatches = []
+            evalResultsSubepoch = np.zeros([ myLiviaNet3D.n_classes, 4 ], dtype="int32")
+    
+            for b_i in xrange(numberBatches):
+                # TODO: Make a line that adds a point at each trained batch (Or percentage being updated)
+                costErrors = myLiviaNet3D.networkModel_Train(b_i, weightsCostFunction)
+                meanBatchCostError = costErrors[0]
+                costsOfBatches.append(meanBatchCostError)
+                myLiviaNet3D.updateLayersMatricesBatchNorm() 
+
+            
+            #======== Calculate and Report accuracy over subepoch
+            meanCostOfSubepoch = sum(costsOfBatches) / float(numberBatches)
+            print(" ---------- Cost of this subEpoch: {}".format(meanCostOfSubepoch))
+            
+            # Release data
+            myLiviaNet3D.trainingData_x.set_value(np.zeros([1,1,1,1,1], dtype="float32"))
+            myLiviaNet3D.trainingData_y.set_value(np.zeros([1,1,1,1], dtype="float32"))
+
+            # Get mean cost epoch
+            costsOfEpoch.append(meanCostOfSubepoch)
+
+        meanCostOfEpoch =  sum(costsOfEpoch) / float(numberOfSubEpochs)
+        
+        # Include the epoch cost to the main training cost and update current mean 
+        trainingCost.append(meanCostOfEpoch)
+        currentMeanCost = sum(trainingCost) / float(str( e_i + 1))
+        
+        print(" ---------- Training on Epoch #" + str(e_i) + " finished ----------" )
+        print(" ---------- Cost of Epoch: {} / Mean training error {}".format(meanCostOfEpoch,currentMeanCost))
+        print(" -------------------------------------------------------- " )
+
+        # ---------------------- Start validation ---------------------- #
+        
+        numberImagesToSegment = len(imageNames_Val)
+        print(" ********************** Starting validation **********************")
+
+        # Run over the images to segment   
+        for i_d in xrange(numberImagesToSegment) :
+            print("-------------  Segmenting subject: {} ....total: {}/{}... -------------".format(names_Val[i_d],str(i_d+1),str(numberImagesToSegment)))
+            strideValues = myLiviaNet3D.lastLayer.outputShapeTest[2:]
+            
+            segmentVolume(myLiviaNet3D,
+                          i_d,
+                          imageNames_Val,  # Full path
+                          names_Val,       # Only image name
+                          groundTruthNames_Val,
+                          roiNames_Val,
+                          imageType,
+                          applyPadding,
+                          receptiveField, 
+                          sampleSize_Train,
+                          strideValues,
+                          myLiviaNet3D.batch_Size,
+                          0 # Validation (0) or testing (1)
+                          )
+                         
+       
+        print(" ********************** Validation DONE ********************** ")
+
+        # ------ In this point the training is done at Epoch n ---------#
+        # Increase number of epochs trained
+        myLiviaNet3D.numberOfEpochsTrained += 1
+
+        #  --------------- Save the model --------------- 
+        BASE_DIR = os.getcwd()
+        path_Temp = os.path.join(BASE_DIR,'outputFiles')
+        netFolderName = os.path.join(path_Temp,myLiviaNet3D.folderName)
+        netFolderName  = os.path.join(netFolderName,'Networks')
+
+        modelFileName = netFolderName + "/" + myLiviaNet3D.networkName + "_Epoch" + str (myLiviaNet3D.numberOfEpochsTrained)
+        dump_model_to_gzip_file(myLiviaNet3D, modelFileName)
+ 
+        strFinal =  " Network model saved in " + netFolderName + " as " + myLiviaNet3D.networkName + "_Epoch" + str (myLiviaNet3D.numberOfEpochsTrained)
+        print  strFinal
+
+    print("................ The whole Training is done.....")
+    print(" ************************************************************************************ ")
