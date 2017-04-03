c0d16d1378a2d7872283c5745828e102d32fe046
Jose Dolz <jose.dolz.upv@gmail.com>
2017-04-02 23:40:37 -0400

    Add files via upload

---
+++ src/LiviaNet/Modules/General/Evaluation.py
@@ -0,0 +1,98 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
+import pdb
+import numpy as np
+
+# ----- Dice Score -----
+def computeDice(autoSeg, groundTruth):
+    """ Returns
+    -------
+    DiceArray : floats array
+          
+          Dice coefficient as a float on range [0,1].
+          Maximum similarity = 1
+          No similarity = 0 """
+          
+    n_classes = int( np.max(groundTruth) + 1)
+   
+    DiceArray = []
+    
+    
+    for c_i in xrange(1,n_classes):
+        idx_Auto = np.where(autoSeg.flatten() == c_i)[0]
+        idx_GT   = np.where(groundTruth.flatten() == c_i)[0]
+        
+        autoArray = np.zeros(autoSeg.size,dtype=np.bool)
+        autoArray[idx_Auto] = 1
+        
+        gtArray = np.zeros(autoSeg.size,dtype=np.bool)
+        gtArray[idx_GT] = 1
+        
+        dsc = dice(autoArray, gtArray, empty_score=1.0)
+
+        #dice = np.sum(autoSeg[groundTruth==c_i])*2.0 / (np.sum(autoSeg) + np.sum(groundTruth))
+        DiceArray.append(dsc)
+        
+    return DiceArray
+
+
+def dice(im1, im2, empty_score=1.0):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+        
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / im_sum
