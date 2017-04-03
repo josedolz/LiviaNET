b455239cd9b6a107c0874cf6f8e485ed302d5a0d
Jose Dolz <jose.dolz.upv@gmail.com>
2017-04-02 23:57:38 -0400

    Add files via upload

---
+++ src/LiviaNet/Modules/NeuralNetwork/ActivationFunctions.py
@@ -0,0 +1,177 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
+import pdb
+import os
+import numpy as np
+import theano
+import theano.tensor as T
+
+import sys
+
+# https://github.com/Theano/Theano/issues/689
+sys.setrecursionlimit(50000)
+
+
+#####################################################
+## Various activation functions for the CNN layers ##
+#####################################################
+
+# Sigmoid activations
+def applyActivationFunction_Sigmoid(inputData):
+    """ inputData is a tensor5D with shape:
+      (batchSize,
+       Number of feature Maps,
+       convolvedImageShape[0],
+       convolvedImageShape[1],
+       convolvedImageShape[2]) """
+    
+    outputData = T.nnet.sigmoid(inputData)
+    return ( outputData )
+
+# Tanh activations
+def applyActivationFunction_Tanh(inputData):
+    """inputData is a tensor5D with shape:
+    # (batchSize,
+    # Number of feature Maps,
+    # convolvedImageShape[0],
+    # convolvedImageShape[1],
+    # convolvedImageShape[2])"""
+    
+    outputData= T.tanh(inputData)
+    return ( outputData )
+
+# *** There actually exist several ways to implement ReLU activations ***
+# --- Version 1 ---  
+def applyActivationFunction_ReLU_v1(inputData):
+    """ inputData is a tensor5D with shape:
+    # (batchSize,
+    # Number of feature Maps,
+    # convolvedImageShape[0],
+    # convolvedImageShape[1],
+    # convolvedImageShape[2]) """
+    
+    return T.maximum(inputData,0)
+
+# --- Version 2 ---    
+def applyActivationFunction_ReLU_v2(inputData):
+
+    return T.switch(inputData < 0., 0., inputData)
+ 
+# --- Version 3 ---
+def applyActivationFunction_ReLU_v3(inputData):
+
+    return ((inputData + abs(inputData))/2.0)
+    
+# --- Version 4 ---
+def applyActivationFunction_ReLU_v3(inputData):
+    
+    return (T.sgn(inputData) + 1) * inputData * 0.5    
+
+# *** LeakyReLU *** 
+def applyActivationFunction_LeakyReLU( inputData, leakiness ) :
+    """leakiness : float
+        Slope for negative input, usually between 0 and 1.
+        A leakiness of 0 will lead to the standard rectifier,
+        a leakiness of 1 will lead to a linear activation function,
+        and any value in between will give a leaky rectifier.
+        
+        [1] Maas et al. (2013):
+        Rectifier Nonlinearities Improve Neural Network Acoustic Models,
+        http://web.stanford.edu/~awni/papers/relu_hybrid_icml2013_final.pdf
+       
+    
+    - The input is a tensor of shape (batchSize, FeatMaps, xDim, yDim, zDim) """
+
+    pos = 0.5 * (1 + leakiness)
+    neg = 0.5 * (1 - leakiness)
+    
+    output = pos * inputData + neg * abs(inputData)
+ 
+    return (output)
+
+# *** There actually exist several ways to implement PReLU activations ***
+
+# PReLU activations (from Kamnitsas)
+def applyActivationFunction_PReLU( inputData, PreluActivations ) :
+    """Parametric Rectified Linear Unit.
+    It follows:
+    `f(x) = alpha * x for x < 0`,
+    `f(x) = x for x >= 0`,
+    where `alpha` is a learned array with the same shape as x.
+    
+    - The input is a tensor of shape (batchSize, FeatMaps, xDim, yDim, zDim) """
+    preluActivationsAsRow = PreluActivations.dimshuffle('x', 0, 'x', 'x', 'x')
+
+    pos = T.maximum(0, inputData)
+    neg = preluActivationsAsRow * (inputData - abs(inputData)) * 0.5
+    output = pos + neg
+
+    return (output)
+
+# --- version 2 ---
+def applyActivationFunction_PReLU_v2(inputData,PreluActivations) :
+    """ inputData is a tensor5D with shape:
+     (batchSize,
+     Number of feature Maps,
+     convolvedImageShape[0],
+     convolvedImageShape[1],
+     convolvedImageShape[2]) """ 
+
+    # The input is a tensor of shape (batchSize, FeatMaps, xDim, yDim, zDim)
+    preluActivationsAsRow = PreluActivations.dimshuffle('x', 0, 'x', 'x', 'x')
+    
+    pos = ((inputData + abs(inputData)) / 2.0 )
+    neg = preluActivationsAsRow * ((inputData - abs(inputData)) / 2.0 )
+    output = pos + neg
+
+    return ( output)
+
+# --- version 3 ---
+def applyActivationFunction_PReLU_v2(inputData,PreluActivations) :
+    """ inputData is a tensor5D with shape:
+     (batchSize,
+     Number of feature Maps,
+     convolvedImageShape[0],
+     convolvedImageShape[1],
+     convolvedImageShape[2]) """ 
+
+    # The input is a tensor of shape (batchSize, FeatMaps, xDim, yDim, zDim)
+    preluActivationsAsRow = PreluActivations.dimshuffle('x', 0, 'x', 'x', 'x')
+    
+    pos = 0.5 * (1 + preluActivationsAsRow )
+    neg = 0.5 * (1 - preluActivationsAsRow )
+    output = pos * inputData + neg * abs(inputData)
+
+    return ( output)
+
+# Benchmark on ReLU/PReLU activations:
+# http://gforge.se/2015/06/benchmarking-relu-and-prelu/
+    
+# TODO. Implement some other activation functions:
+# Ex: Randomized ReLU
+#     S-shape Relu
+#     ThresholdedReLU
---
+++ src/LiviaNet/Modules/NeuralNetwork/layerOperations.py
@@ -0,0 +1,172 @@
+""" 
+Copyright (c) 2016, Jose Dolz .All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+Jose Dolz. Dec, 2016.
+email: jose.dolz.upv@gmail.com
+LIVA Department, ETS, Montreal.
+"""
+
+import theano.tensor as T
+import theano
+import random
+import numpy as np
+
+# ----------------- Apply dropout to a given input ---------------#
+def apply_Dropout(rng, dropoutRate, inputShape, inputData, task) :
+    """ Task:
+    #    0: Training
+    #    1: Validation
+    #    2: Testing """
+    outputData = inputData
+    
+    if dropoutRate > 0.001 : 
+        activationRate = (1-dropoutRate)
+        srng = T.shared_randomstreams.RandomStreams(rng.randint(999999))
+        dropoutMask = srng.binomial(n=1, size=inputShape, p=activationRate, dtype=theano.config.floatX)
+        if task == 0:
+            outputData = inputData * dropoutMask
+        else:
+            outputData = inputData * activationRate
+    return (outputData)
+
+""" Another dropout version """
+""" def applyDropout(rng, inputLayer, inputLayerSize, dropoutRate) :
+    # https://iamtrask.github.io/2015/07/28/dropout/
+    # https://github.com/mdenil/dropout/blob/master/mlp.py
+    
+    #srng = T.shared_randomstreams.RandomStreams(rng.randint(999999))
+    #dropoutMask = srng.binomial(n=1, p= 1-dropoutRate, size=inputLayerSize, dtype=theano.config.floatX)
+    dropoutMask = numpy.random.binomial([numpy.ones((inputLayer.W.eval().shape))],1-dropoutRate)[0] * (1.0/(1-dropoutRate))
+    output = inputLayer.W * dropoutMask
+    return (output)"""
+    
+# ----------------- Convolve an input with a given kernel ---------------#
+def convolveWithKernel(W, filter_shape, inputSample, inputSampleShape) :
+    wReshapedForConv = W.dimshuffle(0,4,1,2,3)
+    wReshapedForConvShape = (filter_shape[0], filter_shape[4], filter_shape[1], filter_shape[2], filter_shape[3])
+
+    #Reshape image for what conv3d2d needs:
+    inputSampleReshaped = inputSample.dimshuffle(0, 4, 1, 2, 3)
+    inputSampleReshapedShape = (inputSampleShape[0],
+                                inputSampleShape[4],
+                                inputSampleShape[1],
+                                inputSampleShape[2],
+                                inputSampleShape[3]) 
+    
+    convolved_Output = T.nnet.conv3d2d.conv3d(inputSampleReshaped, 
+                                              wReshapedForConv,
+                                              inputSampleReshapedShape, 
+                                              wReshapedForConvShape,
+                                              border_mode = 'valid')
+                                        
+    output = convolved_Output.dimshuffle(0, 2, 3, 4, 1) 
+
+    outputShape = [inputSampleShape[0],
+                   filter_shape[0],
+                   inputSampleShape[2]-filter_shape[2]+1,
+                   inputSampleShape[3]-filter_shape[3]+1,
+                   inputSampleShape[4]-filter_shape[4]+1]
+
+    return (output, outputShape)
+
+# ----------------- Apply Batch normalization ---------------#
+""" Apply Batch normalization """
+""" From Kamnitsas """
+def applyBn(numberEpochApplyRolling, inputTrain, inputTest, inputShapeTrain) :
+    numberOfChannels = inputShapeTrain[1]
+    
+    gBn_values = np.ones( (numberOfChannels), dtype = 'float32' )
+    gBn = theano.shared(value=gBn_values, borrow=True)
+    bBn_values = np.zeros( (numberOfChannels), dtype = 'float32')
+    bBn = theano.shared(value=bBn_values, borrow=True)
+    
+    # For rolling average:
+    muArray = theano.shared(np.zeros( (numberEpochApplyRolling, numberOfChannels), dtype = 'float32' ), borrow=True)
+    varArray = theano.shared(np.ones( (numberEpochApplyRolling, numberOfChannels), dtype = 'float32' ), borrow=True)
+    sharedNewMu_B = theano.shared(np.zeros( (numberOfChannels), dtype = 'float32'), borrow=True)
+    sharedNewVar_B = theano.shared(np.ones( (numberOfChannels), dtype = 'float32'), borrow=True)
+    
+    e1 = np.finfo(np.float32).tiny 
+
+    mu_B = inputTrain.mean(axis=[0,2,3,4]) 
+    mu_B = T.unbroadcast(mu_B, (0)) 
+    var_B = inputTrain.var(axis=[0,2,3,4])
+    var_B = T.unbroadcast(var_B, (0))
+    var_B_plusE = var_B + e1
+    
+    #---computing mu and var for inference from rolling average---
+    mu_RollingAverage = muArray.mean(axis=0)
+    effectiveSize = inputShapeTrain[0]*inputShapeTrain[2]*inputShapeTrain[3]*inputShapeTrain[4]
+    var_RollingAverage = (effectiveSize/(effectiveSize-1))*varArray.mean(axis=0)
+    var_RollingAverage_plusE = var_RollingAverage + e1
+    
+    # training
+    normXi_train = (inputTrain - mu_B.dimshuffle('x', 0, 'x', 'x', 'x')) /  T.sqrt(var_B_plusE.dimshuffle('x', 0, 'x', 'x', 'x')) 
+    normYi_train = gBn.dimshuffle('x', 0, 'x', 'x', 'x') * normXi_train + bBn.dimshuffle('x', 0, 'x', 'x', 'x') 
+
+    # testing
+    normXi_test = (inputTest - mu_RollingAverage.dimshuffle('x', 0, 'x', 'x', 'x')) /  T.sqrt(var_RollingAverage_plusE.dimshuffle('x', 0, 'x', 'x', 'x')) 
+    normYi_test = gBn.dimshuffle('x', 0, 'x', 'x', 'x') * normXi_test + bBn.dimshuffle('x', 0, 'x', 'x', 'x')
+    
+    return (normYi_train,
+            normYi_test,
+            gBn,
+            bBn,
+            muArray,
+            varArray,
+            sharedNewMu_B,
+            sharedNewVar_B,
+            mu_B, 
+            var_B
+            )
+
+
+# ----------------- Apply Softmax ---------------#
+def applySoftMax( inputSample, inputSampleShape, numClasses, softmaxTemperature):
+   
+    inputSampleReshaped = inputSample.dimshuffle(0, 2, 3, 4, 1) 
+    inputSampleFlattened = inputSampleReshaped.flatten(1) 
+
+    numClassifiedVoxels = inputSampleShape[2]*inputSampleShape[3]*inputSampleShape[4]
+    firstDimOfinputSample2d = inputSampleShape[0]*numClassifiedVoxels
+    inputSample2d = inputSampleFlattened.reshape((firstDimOfinputSample2d, numClasses)) 
+
+    # Predicted probability per class.
+    p_y_given_x_2d = T.nnet.softmax(inputSample2d/softmaxTemperature)
+    
+    p_y_given_x_class = p_y_given_x_2d.reshape((inputSampleShape[0],
+                                                inputSampleShape[2],
+                                                inputSampleShape[3],
+                                                inputSampleShape[4],
+                                                inputSampleShape[1]))
+                                                    
+    p_y_given_x = p_y_given_x_class.dimshuffle(0,4,1,2,3) 
+
+    y_pred = T.argmax(p_y_given_x, axis=1) 
+    
+    return ( p_y_given_x, y_pred )
+
+# ----------------- Apply Bias to feat maps ---------------#                
+def applyBiasToFeatureMaps( bias, featMaps ) :
+    featMaps = featMaps + bias.dimshuffle('x', 0, 'x', 'x', 'x')
+    
+    return (featMaps)
+
