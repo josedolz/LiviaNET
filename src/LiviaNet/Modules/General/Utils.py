f3ef649984d0a213da0bfcbbfc1bd7565f2c011f
Jose Dolz <jose.dolz.upv@gmail.com>
2017-04-07 20:54:43 -0400

    Update Utils.py
    Modify getImageSet to work with non existing directories

--- src/LiviaNet/Modules/General/Utils.py
+++ src/LiviaNet/Modules/General/Utils.py
@@ -199,20 +199,25 @@ def makeFolder(folderName, display_Str) :
 
 """ Get a set of images from a folder given an array of indexes """
 def getImagesSet(imagesFolder, imageIndexes) :
-   imageNames = [f for f in os.listdir(imagesFolder) if isfile(join(imagesFolder, f))]
-   imageNames.sort()
-   
-   # Remove corrupted files (if any)
-   if '.DS_Store' in imageNames: imageNames.remove('.DS_Store')
-
    imageNamesToGetWithFullPath = []
    imageNamesToGet = []
+   
+   if os.path.exists(imagesFolder):
+       imageNames = [f for f in os.listdir(imagesFolder) if isfile(join(imagesFolder, f))]
+       imageNames.sort()
+   
+       # Remove corrupted files (if any)
+       if '.DS_Store' in imageNames: imageNames.remove('.DS_Store')
+
+       imageNamesToGetWithFullPath = []
+       imageNamesToGet = []
   
-   if ( len(imageNames) > 0):  
-       imageNamesToGetWithFullPath = [join(imagesFolder,imageNames[imageIndexes[i]]) for i in range(0,len(imageIndexes))]
-       imageNamesToGet = [imageNames[imageIndexes[i]] for i in range(0,len(imageIndexes))]
+       if ( len(imageNames) > 0):  
+           imageNamesToGetWithFullPath = [join(imagesFolder,imageNames[imageIndexes[i]]) for i in range(0,len(imageIndexes))]
+           imageNamesToGet = [imageNames[imageIndexes[i]] for i in range(0,len(imageIndexes))]
 
    return (imageNamesToGetWithFullPath,imageNamesToGet)
+
 
 
 """" Get a set of weights from a folder given an array of indexes """
